be2062f17300a8f9280ded1439cfcdd5d9d267d7
Douglas <douglas_9pgt@hotmail.com>
2024-08-18 20:39:41 -0300

    ajustes

--- src/main/java/com/pactoteste/entity/PessoaEntity.java
+++ src/main/java/com/pactoteste/entity/PessoaEntity.java
@@ -14,8 +14,6 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
